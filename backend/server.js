b5d4a2b7d4a2b76fd43d7b6bd9b07ddab6e73bf5
Ankit Chouhan <shankit119@gmail.com>
2024-09-20 15:52:28 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -26,16 +26,16 @@ const domainsFromEnv = process.env.CORS_DOMAINS || ""
 
 const whitelist = domainsFromEnv.split(",").map(item => item.trim())
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin || whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error("Not allowed by CORS"))
-//     }
-//   },
-//   credentials: true,
-// }
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
 app.use(cors())
 
 ////////////
